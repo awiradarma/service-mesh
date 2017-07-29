7d27a03b0a3ca904ca8592ad6a552121b99acf1c
Andre Wiradarma <awiradarma@gmail.com>
2017-07-29 12:12:14 -0500

    removed unnecessary entries

--- setupDedicatedIngress.sh
+++ setupDedicatedIngress.sh
@@ -1,8 +1,6 @@
 oc login -u system:admin
 oc project myproject
-# oc adm policy add-scc-to-user privileged -z default
-oc policy add-role-to-user view -z default
-oc adm policy add-cluster-role-to-user cluster-reader -z default # TODO: try to change to cluster-reader or change namespace for ingress controller to watch only its own ns and remove this line
+oc adm policy add-cluster-role-to-user cluster-reader -z default 
 oc apply -f dedicatedsvcingress.yml
 oc expose svc l5d-admin
 oc expose svc l5d-incoming --hostname api.acme.com
