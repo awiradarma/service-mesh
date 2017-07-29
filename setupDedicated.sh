c448db12671cfd389f9de5c35a0898ab6a0c4d6e
Andre Wiradarma <awiradarma@gmail.com>
2017-07-28 21:55:36 -0500

    Add sample for non daemonset dedicated l5d pod

---
+++ setupDedicated.sh
@@ -0,0 +1,7 @@
+oc login -u system:admin
+oc project myproject
+oc adm policy add-scc-to-user privileged -z default
+oc policy add-role-to-user view -z default
+oc apply -f dedicatedsvc.yml
+oc expose svc l5d-admin
+oc expose svc l5d-incoming
