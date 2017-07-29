f2b805e63e72342872838ead0dc57ed8526f3ab3
Andre Wiradarma <awiradarma@gmail.com>
2017-07-29 11:58:54 -0500

    remove scc constraint for dedicated ingress setup

--- setupDedicatedIngress.sh
+++ setupDedicatedIngress.sh
@@ -1,6 +1,6 @@
 oc login -u system:admin
 oc project myproject
-oc adm policy add-scc-to-user privileged -z default
+# oc adm policy add-scc-to-user privileged -z default
 oc policy add-role-to-user view -z default
 oc adm policy add-cluster-role-to-user cluster-reader -z default # TODO: try to change to cluster-reader or change namespace for ingress controller to watch only its own ns and remove this line
 oc apply -f dedicatedsvcingress.yml
