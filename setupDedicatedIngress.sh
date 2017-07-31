6b412086a6856f994dd26d586a528f15145647e5
Andre Wiradarma <awiradarma@gmail.com>
2017-07-31 16:52:17 -0500

    Modified the outgoing dtab to try resolving to the right l5d-incoming service based on projectname.acme.com naming convention

--- setupDedicatedIngress.sh
+++ setupDedicatedIngress.sh
@@ -3,4 +3,7 @@ oc project myproject
 oc adm policy add-cluster-role-to-user cluster-reader -z default 
 oc apply -f dedicatedsvcingress.yml
 oc expose svc l5d-admin
-oc expose svc l5d-incoming --hostname api.acme.com
+oc expose svc l5d-incoming --hostname myproject.acme.com
+# create an entry in /etc/hosts for myproject.acme.com
+# pointing to the exposed IP address of the l5d-incoming route
+# then use curl myproject.acme.com/rest/hello to test it out
