bf6f9ff26244714e6147025dfac1ba495eb5810b
Andre Wiradarma <awiradarma@gmail.com>
2018-07-12 14:33:52 -0500

    adding examples using v1alphav3 syntax

--- multiproject_istio/setupCrossProject.sh
+++ multiproject_istio/setupCrossProject.sh
@@ -1,14 +1,14 @@
-oc login -u system:admin
-oc new-project hello
-oc project hello
-oc label namespace hello istio-injection=enabled
-oc adm policy add-scc-to-user privileged -z default -n hello
-oc apply -f hello.yml
-oc apply -f ingress_hello.yml
-oc new-project world
-oc project world
-oc label namespace world istio-injection=enabled
-oc adm policy add-scc-to-user privileged -z default -n world
-oc apply -f world.yml
-oc apply -f ingress_world.yml
-oc get route -n istio-system
+oc2 login -u system:admin
+oc2 new-project hello
+oc2 project hello
+#oc label namespace hello istio-injection=enabled
+#oc2 adm policy add-scc-to-user privileged -z default -n hello
+oc2 apply -f hello.yml
+#oc2 apply -f ingress_hello.yml
+oc2 new-project world
+oc2 project world
+#oc label namespace world istio-injection=enabled
+#oc2 adm policy add-scc-to-user privileged -z default -n world
+oc2 apply -f world.yml
+#oc2 apply -f ingress_world.yml
+#oc2 get route -n istio-system
