630f22223106bb517a1b31c6a4fb36b773cc13ed
Andre Wiradarma <awiradarma@gmail.com>
2018-07-30 18:21:56 -0500

    modified to remove duplicate host entries

--- multiproject_istio/setupCrossProject.sh
+++ multiproject_istio/setupCrossProject.sh
@@ -1,14 +1,14 @@
-oc2 login -u system:admin
-oc2 new-project hello
-oc2 project hello
+oc3 login -u system:admin
+oc3 new-project hello
+oc3 project hello
 #oc label namespace hello istio-injection=enabled
 #oc2 adm policy add-scc-to-user privileged -z default -n hello
-oc2 apply -f hello.yml
+oc3 apply -f hello.yml
 #oc2 apply -f ingress_hello.yml
-oc2 new-project world
-oc2 project world
+oc3 new-project world
+oc3 project world
 #oc label namespace world istio-injection=enabled
 #oc2 adm policy add-scc-to-user privileged -z default -n world
-oc2 apply -f world.yml
+oc3 apply -f world.yml
 #oc2 apply -f ingress_world.yml
 #oc2 get route -n istio-system
